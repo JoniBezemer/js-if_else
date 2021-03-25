f13192c3d21f98d48339d5eeadefd64813551dfa
JoniBezemer <77683711+JoniBezemer@users.noreply.github.com>
2021-03-25 16:15:57 +0100

    Add files via upload

---
+++ 4-if_else/script.js
@@ -0,0 +1,25 @@
+var age= 18;
+
+if (age > 17){
+
+    document.write("Je mag alchohol drinken");
+
+} else{
+
+    document.write("Je mag geen alchohol drinken");
+}
+
+
+
+var isFemale= true;
+
+if(isFemale){
+    document.write("Je mag gratis naar binnen")
+}else{
+    document.write("Koop hier je ticket")
+}
+
+
+var driverStatus= 'bob';
+
+
